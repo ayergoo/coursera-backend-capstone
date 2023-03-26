2b92bc85bf52705e8604a8428c5b4be61704d30c
aleks <ayergoo@gmail.com>
2023-03-26 20:21:20 +0200

    views, serializers, urls

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path
+from .views import *
 
 
-
-
-url_patters = []
+urlpatterns = [
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView 
+
+from .models import *
+from .serializers import *
+# from django.shortcuts import render
+
+# Create your views here. 
+class MenuItemsView(ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
 # Create your views here.
+class BookingView(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MenuItemSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success", "data":serializer.data})
+        
+
+# class MenuItemsView(ListCreateAPIView):
+
+#     def get(self, request):
+#         items = Menu.objects.all()
+#         serializer = MenuItemSerializer(items, many=True)
+#         return Response(serializer.data)
+    
