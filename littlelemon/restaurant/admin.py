6838779aaae231066fc60d625fa80d1992447663
aleks <ayergoo@gmail.com>
2023-03-26 23:18:44 +0200

    booking form

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -3,4 +3,4 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Booking)
-admin.site.register(MenuItem)
+admin.site.register(Menu)
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,16 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
 class Booking(models.Model):
-    name = models.CharField(max_length=255)
-    no_of_guests = models.SmallIntegerField()
-    booking_date = models.DateTimeField()
+    first_name = models.CharField(max_length=200)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self): 
+        return self.first_name
 
 
-class MenuItem(models.Model):
-    title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.SmallIntegerField()
+# Add code to create Menu model
+class Menu(models.Model):
+   name = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   menu_item_description = models.TextField(max_length=1000, default='') 
+
+   def __str__(self):
+      return self.name
+
+
+
+# from django.db import models
+
+# # Create your models here.
+# class Booking(models.Model):
+#     name = models.CharField(max_length=255)
+#     no_of_guests = models.SmallIntegerField()
+#     booking_date = models.DateTimeField()
+
+
+# class MenuItem(models.Model):
+#     title = models.CharField(max_length=255)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     inventory = models.SmallIntegerField()
     
-    def get_item(self):
-        return f'{self.title} : {str(self.price)}'
+#     def get_item(self):
+#         return f'{self.title} : {str(self.price)}'
